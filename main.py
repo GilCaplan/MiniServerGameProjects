042d3e8b60ae13f46db7d5692502e5542412b7d9
GilCaplan <rockycaplan@gmail.com>
2023-04-11 17:33:44 +0300

    made a main

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-import dixit, chess, Tictactoe, boggle, MasterMind
+import dixit
+import chess
+import Tictactoe
+import boggle
+import MasterMind
+
 if __name__ == '__main__':
-    dixit.run_game()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    Game = "play"
+    while Game == "play":
+        Game = input("enter game, enter exact spelling: dixit, chess, Tictactoe, boggle, MasterMind: ")
+        if Game == "dixit":
+            dixit.run_game()
+            Game = "stop"
+            break
+        elif Game == "chess":
+            chess.run_game()
+            Game = "stop"
+            break
+        elif Game == "Tictactoe":
+            Tictactoe.run_game()
+            Game = "stop"
+            break
+        elif Game == "boggle":
+            boggle.run_game()
+            Game = "stop"
+            break
+        elif Game == "MasterMind":
+            MasterMind.run_game()
+            Game = "stop"
+            break
+        elif Game == "exit":
+            exit()
+        else:
+            print("error: enter game name or type exit to finish program")
