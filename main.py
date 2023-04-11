306697792dfb59352e44cd86ccb357eba6386f00
GilCaplan <rockycaplan@gmail.com>
2023-04-11 17:44:21 +0300

    can run from the main

--- main.py
+++ main.py
@@ -4,31 +4,25 @@ import Tictactoe
 import boggle
 import MasterMind
 
+
+def play_game():
+    game = input("enter game, enter exact spelling: dixit, chess, Tictactoe, boggle, MasterMind: ")
+    if game == "dixit":
+        dixit.run_game()
+    elif game == "chess":
+        chess.run_game()
+    elif game == "Tictactoe":
+        Tictactoe.run_game()
+    elif game == "boggle":
+        boggle.run_game()
+    elif game == "MasterMind":
+        MasterMind.run_game()
+    elif game == "exit":
+        exit()
+    else:
+        print("error: enter game name or type exit to finish program")
+        return "rerun"
+    return "done"
+
 if __name__ == '__main__':
-    Game = "play"
-    while Game == "play":
-        Game = input("enter game, enter exact spelling: dixit, chess, Tictactoe, boggle, MasterMind: ")
-        if Game == "dixit":
-            dixit.run_game()
-            Game = "stop"
-            break
-        elif Game == "chess":
-            chess.run_game()
-            Game = "stop"
-            break
-        elif Game == "Tictactoe":
-            Tictactoe.run_game()
-            Game = "stop"
-            break
-        elif Game == "boggle":
-            boggle.run_game()
-            Game = "stop"
-            break
-        elif Game == "MasterMind":
-            MasterMind.run_game()
-            Game = "stop"
-            break
-        elif Game == "exit":
-            exit()
-        else:
-            print("error: enter game name or type exit to finish program")
+    status = play_game()
